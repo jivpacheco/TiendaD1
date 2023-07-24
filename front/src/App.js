990d82d2e6902463690b1897380c8f0b69921706
jivpacheco <jivpacheco@hotmail.com>
2023-07-24 13:19:22 -0400

    definiendo rutas del front

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,31 @@ import React from 'react';
 import Header from './componentes/layout/Header';
 import Footer from './componentes/layout/Footer';
 import Home from './componentes/Home';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Routes from 'react-router-dom';
+// import Route from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-    <Header />
-    <Home />
-    <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className='container container-fluid' >
+          <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path="/Home" element={<Home />}/>
+
+          </Routes>
+        </div>
+        {/* <Home /> */}
+        <Footer />
+
+      </div>
+    </Router>
+
   );
 }
 
