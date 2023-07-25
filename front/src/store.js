b754ffe7873b6a47a6530fdc39fe561c378c4a0f
Jose Valencia <jivpacheco@hotmail.com>
2023-07-24 23:57:53 -0400

    configurando redux

--- front/src/store.js
+++ front/src/store.js
@@ -1,5 +1,6 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const reducer = combineReducers({});
 
@@ -7,6 +8,10 @@ let initialState={};
 
 const middleware = [thunk];
 
-const store = create (reducer, initialState, )
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+// const store = create (reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+// const store = createStore (reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
 
 
