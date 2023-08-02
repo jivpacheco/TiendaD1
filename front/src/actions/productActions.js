2d0e7540d31f9479a933dd15b9be56a60160b332
Jose Valencia <jivpacheco@hotmail.com>
2023-08-01 22:47:24 -0400

    primer contacto del front con el back

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -6,6 +6,7 @@ export const getProducts = () => async (dispatch) => {
     try {
         dispatch({ type: ALL_PRODUCTS_REQUEST })
         const {data} = await axios.get('api/productos');
+      
 
         dispatch({
             type: ALL_PRODUCTS_SUCCESS,
