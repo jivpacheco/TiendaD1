324f2b1829539450af22c3e6d68cc0166fb27152
Jose Valencia <jivpacheco@hotmail.com>
2023-08-13 19:37:17 -0400

    modelando usuarios

--- back/app.js
+++ back/app.js
@@ -5,8 +5,12 @@ const errorMiddleware = require('./middleware/errors')
 app.use(express.json());
 
 const productos = require('./routes/productsRoute');
+const usuarios = require('./routes/authRoute')
+
 
 app.use('/api', productos);
+app.use('/api',usuarios);
+
 
 //Middleware para manejar errores 
 app.use(errorMiddleware);
---
+++ back/controllers/authController.js
@@ -0,0 +1,26 @@
+const User = require('../models/auth.js')
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middleware/catchAsyncErrors')
+
+//Registrar un nuevo usuario /api/registrar
+
+exports.registroUsuario = catchAsyncErrors(async(req, res, next) =>{
+
+    const {nombre, email, password} = req.body;
+   
+    const user = await User.create({
+        nombre,
+        email,
+        password,
+        avatar:{
+            public_id:"370-456322",
+            url: "https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp"
+        }
+    })
+    
+    res.status(201).json({
+        success: true,
+        user
+    })
+    
+})
